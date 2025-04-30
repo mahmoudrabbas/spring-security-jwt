a90f3e243125c5764cbe9a920bafc1a6a4905430
mahmoudrabbas <mahmoudrabass11@gmail.com>
2025-04-30 21:16:15 +0300

    add cors

--- src/main/java/com/workshop/security/config/SecurityConfig.java
+++ src/main/java/com/workshop/security/config/SecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableMethodSecurity
@@ -29,6 +31,19 @@ public class SecurityConfig {
 //    public JwtAuthTokenFilter authenticationJwtTokenFilter(){
 //        return new JwtAuthTokenFilter();
 //    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
